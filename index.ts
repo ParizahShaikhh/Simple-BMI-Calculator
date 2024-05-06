52f13bc3cfa274d9141efc8d865bc51a402d8c27
Parizah Shaikh <parivian6@gmail.com>
2024-05-06 19:27:34 +0500

    CLI-based-BMI-Calculator-Project.

---
+++ index.ts
@@ -0,0 +1,33 @@
+import inquirer from "inquirer";
+
+async function calculateBMI() {
+    let { weight, height } = await inquirer.prompt(
+        [
+            {
+                type: "input",
+                name: "weight",
+                message: "Enter your weight in kilograms:",
+                validate: (input: string) => !isNaN(parseFloat(input)) || "Please enter a valid weight.",
+            },
+            {
+                type: "input",
+                name: "height",
+                message: "Enter your height in meters:",
+                validate: (input: string) => !isNaN(parseFloat(input)) || "Please enter a valid height.",
+            }
+        ]
+    );
+    let bmi = parseFloat(weight) / (parseFloat(height) * parseFloat(height));
+    console.log(`Your BMI is: ${bmi.toFixed(2)}`);
+
+    if (bmi < 18.5) {
+        console.log("You are Underweight.");
+    } else if (bmi >= 18.5 && bmi < 25) {
+        console.log("You have a Normal weight.");
+    } else if (bmi >= 25 && bmi < 30) {
+        console.log("You are Over weight.");
+    } else {
+        console.log("You are Obese.");
+    }
+}
+calculateBMI();
